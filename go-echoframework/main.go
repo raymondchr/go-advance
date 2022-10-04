b56fb7cadb4113d71c635b497e972c9d6a05b100
Raymond <raycuffed@gmail.com>
2022-10-04 15:54:32 +0700

    added multiple query parsing method

--- go-echoframework/main.go
+++ go-echoframework/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
@@ -17,5 +18,27 @@ func main() {
 		return ctx.String(http.StatusOK, data)
 	})
 
+	r.GET("/page1", func(ctx echo.Context) error {
+		name := ctx.QueryParam("name")
+		data := fmt.Sprintf("Hello %s", name)
+
+		return ctx.String(http.StatusOK, data)
+	})
+
+	r.GET("/page2/:name", func(ctx echo.Context) error {
+		name := ctx.Param("name")
+		data := fmt.Sprintf("Hello %s", name)
+
+		return ctx.String(http.StatusOK, data)
+	})
+
+	r.GET("/page3/:name/*", func(ctx echo.Context) error {
+		name := ctx.Param("name")
+		message := ctx.Param("*")
+
+		data := fmt.Sprintf("Hello %s, I have message for you: %s", name, message)
+
+		return ctx.String(http.StatusOK, data)
+	})
 	r.Start(":9000")
 }
